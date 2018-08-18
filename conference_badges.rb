d3ed3af73b79dcd58bd6dbaa2e8d626bcf07d0bd
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-18 06:01:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  
+  attendees.each do
+    badge_maker(name)
+  end
 end
 
-def assign_rooms(speakers)
+def assign_rooms(attendees)
   
 end
